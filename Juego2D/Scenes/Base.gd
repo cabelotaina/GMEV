136c8c265aacb60a655d7941232322fbb3fbf53e
dSerena <daniels_s_24@hotmail.com>
2019-11-10 20:59:49 +0100

    UI, fondo grande, limites actualizados

--- Juego2D/Scenes/Base.gd
+++ Juego2D/Scenes/Base.gd
@@ -3,14 +3,48 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var UI = false
+var camera1 = false
+var camera2 = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$CameraGlobal.current = false
+	$CameraGlobal.visible = false
+	var camera = $Catfish/CameraCatfish
+	camera.current = true
+	camera.visible = true
 
 func _process(delta):
 	pass
 	
+func _input(event):	
+	if event.is_action_pressed("F1_camera1"):
+		$CameraGlobal.current = true
+		$CameraGlobal.visible = true
+		var camera = $Catfish/CameraCatfish
+		camera.current = false
+		camera.visible = false
+		camera2 = not camera1
+		camera1 = not camera1
+	elif event.is_action_pressed("F2_camera2"):
+		$CameraGlobal.current = false
+		$CameraGlobal.visible = false
+		var camera = $Catfish/CameraCatfish
+		camera.current = true
+		camera.visible = true
+		camera2 = not camera1
+		camera1 = not camera1
+	elif event.is_action_pressed("F3_UI"):
+		print("UIT", UI)
+		
+		if UI == false:
+			if camera1 == true:
+				$CameraGlobal/UI.visible = true
+			elif camera2 == true:
+				$Catfish/CameraCatfish/UI.visible = true
+		UI = not UI
+		print("UIT", UI)
+			
 func create_bubbles():
 	pass
 	
